e5c48e9457ba8a0d2278d1f1849aecf863f3e27c
Shevin Vinoy <94979452+Being-Altruistic@users.noreply.github.com>
2022-10-28 16:41:52 +0530

    Update settings.py

--- yumstop/yumstop/settings.py
+++ yumstop/yumstop/settings.py
@@ -19,11 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%0j&on5h&ixixo_0ypgs3fu)tzn4h9a%$w901n-)h*ixq=84q^'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
